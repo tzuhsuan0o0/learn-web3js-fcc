74aacd5fb123b2d289ff4b21f083b11a8f42e811
tzuhsuanchangX <tzuhsuanchang@proton.me>
2023-07-06 21:54:11 +0800

    smart contract deployed

--- hardhat-simple-storage-fcc/scripts/deploy.js
+++ hardhat-simple-storage-fcc/scripts/deploy.js
@@ -1,33 +1,51 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+// imports
+const { ethers, run, network } = require("hardhat")
 
+// async main
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
+    console.log("Deploying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.waitForDeployment()
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+    console.log(`Deployed contract to : ${await simpleStorage.getAddress()}`)
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY){
+        await simpleStorage.deploymentTransaction().wait(6)
+        await verify(await simpleStorage.getAddress(), [])
+    }
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+    
+    // Update the current value
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+async function verify(contractAddress, args) {
+    console.log("Verifying contrac...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")){
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+//main
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
